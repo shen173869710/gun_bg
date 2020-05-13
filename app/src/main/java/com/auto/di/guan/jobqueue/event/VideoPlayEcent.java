21a6e68db26806e3b9d938e2bcff7cb1cc624e6a
shen173869710 <469758936>
2020-05-13 21:23:12 +0800

    提交bug修改

---
+++ app/src/main/java/com/auto/di/guan/jobqueue/event/VideoPlayEcent.java
@@ -0,0 +1,12 @@
+package com.auto.di.guan.jobqueue.event;
+
+/**
+ *   设备报警铃声
+ */
+public class VideoPlayEcent {
+    int type;
+
+    public VideoPlayEcent(int type) {
+        this.type = type;
+    }
+}
