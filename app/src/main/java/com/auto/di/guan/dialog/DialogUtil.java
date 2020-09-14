513a33d11f6ceee6ed8355e45cbaa3590e57a0a0
173869710@qq.com <shen469758936>
2020-09-14 14:55:01 +0800

    提交水泵控制相关代码

--- app/src/main/java/com/auto/di/guan/dialog/DialogUtil.java
+++ app/src/main/java/com/auto/di/guan/dialog/DialogUtil.java
@@ -26,4 +26,26 @@ public class DialogUtil {
         content.ok = "确认开始";
         OptionDialog.show(context,content,onDialogClick);
     }
+
+    /**
+     *   开启水泵
+     */
+    public static void startSocket(Context context, String name,OnDialogClick onDialogClick) {
+        DialogContent content = new DialogContent();
+        content.desc = "是否开启"+name;
+        content.cancle = "取消";
+        content.ok = "开启";
+        OptionDialog.show(context,content,onDialogClick);
+    }
+
+    /**
+     *   关闭水泵
+     */
+    public static void closeSocket(Context context, String name,OnDialogClick onDialogClick) {
+        DialogContent content = new DialogContent();
+        content.desc = "是否关闭"+name;
+        content.cancle = "取消";
+        content.ok = "关闭";
+        OptionDialog.show(context,content,onDialogClick);
+    }
 }
--- app/src/main/java/com/auto/di/guan/fragment/FragmentTab6.java
+++ app/src/main/java/com/auto/di/guan/fragment/FragmentTab6.java
@@ -1,85 +1,226 @@
 package com.auto.di.guan.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.auto.di.guan.R;
 import com.auto.di.guan.adapter.PumpLeftAdapter;
-import com.auto.di.guan.entity.Entiy;
-import com.auto.di.guan.entity.PumpInfo;
+import com.auto.di.guan.dialog.DialogUtil;
+import com.auto.di.guan.dialog.OnDialogClick;
+import com.auto.di.guan.socket.SocketEntiy;
+import com.auto.di.guan.socket.SocketResult;
+import com.auto.di.guan.socket.UdpReceiveThread;
+import com.auto.di.guan.socket.UdpSendThread;
+import com.auto.di.guan.utils.LogUtils;
 import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.listener.OnItemClickListener;
+import com.chad.library.adapter.base.listener.OnItemChildClickListener;
+import com.google.gson.Gson;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
+ *
+ *            //                     项目ID 泵名称
+ * //      关        byte[] buf = "gb 20000   0               ".getBytes();
+ *   //    开        byte[] buf = "kb 20000   0               ".getBytes();
+ * //            byte[] buf = "cb 20000 0               ".getBytes();
+ *
+ *  *                   0    不查询
+ *  *                   1 3  1分钟
+ *  *                   2    10分钟
+ *
  */
 public class FragmentTab6 extends BaseFragment {
-	private RecyclerView list;
-	private PumpLeftAdapter adapter;
-	private View view;
-	private List<PumpInfo> infos = new ArrayList<>();
+	private final String TAG = "FragmentTab6";
 
-	private Button pump_open;
-	private Button pump_close;
-	private TextView pump_pram_1;
-	private TextView pump_pram_2;
-	private TextView pump_pram_3;
-	private TextView pump_pram_4;
+	private View view;
+	private PumpLeftAdapter adapter;
+	private ArrayList<SocketResult> infos = new ArrayList<>();
+	private RecyclerView list;
+
+	private boolean isStart = false;
+
+
+	private android.os.Handler handler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			if (msg.obj == null) {
+				return;
+			}
+
+			LogUtils.e(TAG, "handle what = "+msg.what);
+			if (msg.what == SocketEntiy.STATUS_2_HANDLER) {
+				SocketResult result = (SocketResult) msg.obj;
+				new UdpSendThread(SocketEntiy.getSockenRead(result.getNameCode())).start();
+			}else if (msg.what == SocketEntiy.STATUS_1_3_HANDLER) {
+				SocketResult result = (SocketResult) msg.obj;
+				new UdpSendThread(SocketEntiy.getSockenRead(result.getNameCode())).start();
+			}
+
+		}
+	};
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		view = inflater.inflate(R.layout.fragment_6, null);
-		list = view.findViewById(R.id.pump_list);
-
-		pump_open = view.findViewById(R.id.pump_open);
-		pump_close = view.findViewById(R.id.pump_close);
-		pump_pram_1 = view.findViewById(R.id.pump_pram_1);
-		pump_pram_2 = view.findViewById(R.id.pump_pram_2);
-		pump_pram_3 = view.findViewById(R.id.pump_pram_3);
-		pump_pram_4 = view.findViewById(R.id.pump_pram_4);
-
-
+		EventBus.getDefault().register(this);
+		SocketResult result = new SocketResult();
+		result.setName("水泵0");
+		result.setNameCode("0");
+		result.setVoltage("电压");
+		result.setElectricity("电流");
+		result.setStatus("状态");
+		result.setErrorCode("错误");
+		result.setVoltageValue("");
+		result.setElectricityValue("");
+		result.setStatusValue("");
+		result.setErrorCodeValue("");
+		SocketResult result1 = new SocketResult();
+		result1.setName("水泵1");
+		result1.setNameCode("1");
+		result1.setVoltage("电压");
+		result1.setElectricity("电流");
+		result1.setStatus("状态");
+		result1.setErrorCode("错误");
+		result1.setVoltageValue("");
+		result1.setElectricityValue("");
+		result1.setStatusValue("");
+		result1.setErrorCodeValue("");
+		infos.add(result);
+		infos.add(result1);
+		list = view.findViewById(R.id.fragment_6_list);
 		list.setLayoutManager(new LinearLayoutManager(activity));
-		for (int i = 1; i < 6; i++) {
-			PumpInfo info = new PumpInfo();
-			info.setPumpIndex(i);
-			info.setPumpName("水泵" + i);
-			infos.add(info);
-		}
 		adapter = new PumpLeftAdapter(infos);
 		list.setAdapter(adapter);
-		setData(infos.get(0));
-		adapter.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-				setData(infos.get(position));
-			}
-		});
+		setData();
+
+		new UdpSendThread(SocketEntiy.getSockenRead(result.getNameCode())).start();
+		new UdpSendThread(SocketEntiy.getSockenRead(result1.getNameCode())).start();
 		return view;
 	}
 
 
-	public void setData(PumpInfo info) {
-		pump_open.setText(info.getPumpName() + "开启");
-		pump_close.setText(info.getPumpName() + "关闭");
-		pump_pram_1.setText(Entiy.PUMP_PRAM[0]+ "");
-		pump_pram_2.setText(Entiy.PUMP_PRAM[1] + "");
-		pump_pram_3.setText(Entiy.PUMP_PRAM[2] + "");
-		pump_pram_4.setText(Entiy.PUMP_PRAM[3] + "");
+	public void setData() {
+		adapter.addChildClickViewIds(R.id.item_open, R.id.item_close, R.id.item_1_value);
+		adapter.setOnItemChildClickListener(new OnItemChildClickListener() {
+			@Override
+			public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+				SocketResult result = infos.get(position);
+				int id = view.getId();
+				if (id == R.id.item_open) {
+					DialogUtil.startSocket(getActivity(), result.getName(), new OnDialogClick() {
+						@Override
+						public void onDialogOkClick(String value) {
+							new UdpSendThread(SocketEntiy.getSockenOpen(result.getNameCode())).start();
+						}
+
+						@Override
+						public void onDialogCloseClick(String value) {
+
+						}
+					});
+
+				}else if (id == R.id.item_close) {
+					DialogUtil.closeSocket(getActivity(), result.getName(), new OnDialogClick() {
+						@Override
+						public void onDialogOkClick(String value) {
+							new UdpSendThread(SocketEntiy.getSockenClose(result.getNameCode())).start();
+						}
+
+						@Override
+						public void onDialogCloseClick(String value) {
+
+						}
+					});
+				}else if (id == R.id.item_1_value) {
+					//new UdpSendThread(SocketEntiy.getSockenRead(result.getNameCode())).start();
+				}
+			}
+		});
+
+		new UdpReceiveThread().start();
 	}
 
 	@Override
 	public void refreshData() {
 
+		LogUtils.e(TAG, "refreshData");
+		if (handler != null) {
+			handler.removeMessages(SocketEntiy.STATUS_1_3_HANDLER);
+			handler.removeMessages(SocketEntiy.STATUS_2_HANDLER);
+		}
+
+		int size = infos.size();
+		for (int i = 0; i < size; i++) {
+			SocketResult result = infos.get(i);
+			new UdpSendThread(SocketEntiy.getSockenRead(result.getNameCode())).start();
+		}
+	}
+
+
+	/**
+	 *        自动轮灌组状态更新
+	 * @param event
+	 */
+	@Subscribe(threadMode = ThreadMode.MAIN)
+	public void onSocketEvent(SocketResult event) {
+		if (event != null) {
+			LogUtils.e(TAG, "收到数据" + new Gson().toJson(event));
+		}
+
+		int size = infos.size();
+		for (int i = 0; i < size; i++) {
+			SocketResult result = infos.get(i);
+			if (result.getNameCode().equals(event.getNameCode())) {
+				result.setStatusValue(event.getStatusValue());
+				result.setVoltageValue(event.getVoltageValue());
+				result.setElectricityValue(event.getElectricityValue());
+				result.setErrorCodeValue(event.getErrorCodeValue());
+
+				if (adapter != null) {
+					adapter.notifyDataSetChanged();
+				}
+
+				if ("1".equals(result.getStatusValue())
+						|| "3".equals(result.getStatusValue())) {
+					Message message = new Message();
+					message.what = SocketEntiy.STATUS_1_3_HANDLER;
+					message.obj = result;
+					if (handler != null) {
+						handler.sendMessageDelayed(message, SocketEntiy.STATUS_1_3);
+					}
+				}else if ("2".equals(result.getStatusValue())) {
+					Message message = new Message();
+					message.what = SocketEntiy.STATUS_2_HANDLER;
+					message.obj = result;
+					if (handler != null) {
+						handler.sendMessageDelayed(message, SocketEntiy.STATUS_2);
+					}
+				}
+
+			}
+		}
+	}
+
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		if (handler != null) {
+			handler.removeMessages(SocketEntiy.STATUS_1_3_HANDLER);
+			handler.removeMessages(SocketEntiy.STATUS_2_HANDLER);
+		}
+		EventBus.getDefault().unregister(this);
 	}
 }
